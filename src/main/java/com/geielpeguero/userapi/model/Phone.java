3ab3eccfb93165c7a7aa61e5a13c32ace9bdbefd
geiel <geielpeguero@gmail.com>
2022-10-25 11:47:00 -0400

    Create User and Phone models

---
+++ src/main/java/com/geielpeguero/userapi/model/Phone.java
@@ -0,0 +1,51 @@
+package com.geielpeguero.userapi.model;
+
+import javax.persistence.*;
+
+@Table
+@Entity(name = "phones")
+public class Phone {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Integer cityCode;
+
+    private Integer countryCode;
+
+    @ManyToOne
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(Integer cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public Integer getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(Integer countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
